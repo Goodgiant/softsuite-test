e709c873e64dde39914ecdc0cf1576683e0fe084
Victor Akiti <izu.akiti@gmail.com>
2024-02-08 01:25:57 +0100

    change ellipsis icon in table action

--- src/components/Table/RowActionButton.tsx
+++ src/components/Table/RowActionButton.tsx
@@ -1,12 +1,11 @@
+import { EllipsisOutlined } from "@ant-design/icons";
 import "./Table.scss";
 
 const RowActionButton = () => {
 
     return (
         <div className="row-action-button" onClick={()=>{}}>
-            <div className="round-green-dot"></div>
-            <div className="round-green-dot"></div>
-            <div className="round-green-dot"></div>
+            <EllipsisOutlined />
         </div>
     )
 }
